d4ad01a5796ad2d5dc39fe6f002b3afcfc8e6be7
Dysie Nacpil <dysielnacpil@gmail.com>
2025-02-12 17:23:31 +0000

    2 files

---
+++ part2/nonPrimitive.js
@@ -0,0 +1,21 @@
+const username = {
+    firstname: "hitesh",
+    //"first name": "hitesh"
+    isLoggedin: true,
+};
+
+username.firstname = "D"
+username.lastname = "Nacpil"
+
+console.log(username.firstname);
+//console.log(username."first name");
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);
+
+let today = new Date();
+console.log(today);
+
+//Array
+
+let heros = ["a", "b", "c"];
---
+++ part2/primitive.js
@@ -0,0 +1,10 @@
+//Number
+
+let balance = 120;
+let anotherBalance = new Number(120);
+
+console.log(balance);
+console.log(anotherBalance.valueOf);
+
+console.log(typeof balance);
+
